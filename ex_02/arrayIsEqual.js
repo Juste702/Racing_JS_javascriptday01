07a24786b45edfe77226054d4ae5444b83d9aec8
Juste702 <juste.bidouzo@epitech.eu>
2025-09-09 15:28:57 +0100

    Add arrayIsEqual.js file

---
+++ ex_02/arrayIsEqual.js
@@ -0,0 +1,11 @@
+function arrayIsEqual(array1, array2){
+	if ( array1.length !== array2.length) return false;
+	
+	for (let i = 0; i < array1.length; i++){
+		if (array1[i] !== array2[i]) return false;
+	}
+	return true;
+}
+const a = [1, 2];
+const b = [3, 4];
+console.log(arrayIsEqual(a,b)); 
